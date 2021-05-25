ace3eca7242c289c8321288094b8e002d9dc6b21
IgorTikush <igortikush@mail.ru>
2021-05-26 00:20:45 +1000

    added category controller and some style

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,38 +1,138 @@
-const Categoty = require('../models/category');
+const Category = require('../models/category');
+const Coin = require('../models/coin')
 const async = require('async');
 const { body,validationResult } = require('express-validator');
 
+
+
 exports.index = function(req, res, next) {
-    res.send('to be implemented')
+    Category.find()
+        .exec(function (err, category_list){
+            if (err) {return next(err)}
+            res.render('index', {category_list: category_list, title: 'Crypto categories'})
+        })
 }
 exports.category_create_get = function(req,res, next) {
-    res.send('to be implemented')
+    res.render('category_form', {title: 'Create category'})
 }
 
-exports.category_create_post = function(req,res, next) {
-    res.send('to be implemented')
-}
+exports.category_create_post = [
+    //Validate and sanitize all fields
+    body('name', 'Name is required').trim().isLength({min: 3}).escape(),
+    body('description', 'Description is required').trim().isLength({min: 10}).escape(),
+    //After validation continue
+    (req, res, next) => {
+        //Extract validation errors
+        const errors = validationResult(req);
+        
+        //Create new category instance
+        let category = new Category({
+            name: req.body.name,
+            description: req.body.description
+        })
+        if(!errors.isEmpty()) {
+            //there are errors, render form again with sanitazed values
+            res.render('category_form', {title: 'Create category', category: category, action: 'Create'})
+            console.log(errors)
+            return
+        } else {
+            //Data from form is valid
+            category.save(function(err) {
+                if(err) { return next(err); }
+                //Success
+                res.redirect('/catalog')
+            })
+        }
+        
+    }
+
+]
 
 exports.category_update_get = function(req,res, next) {
-    res.send('to be implemented')
+    Category.findById(req.params.id)
+        .exec(function(err, category) {
+            if (err) {return next(err)}
+            res.render('category_form', {title: 'Update category', category: category, action: 'Update'})
+        })
 }
 
-exports.category_update_post = function(req,res, next) {
-    res.send('to be implemented')
-}
+exports.category_update_post = [
+    //Validate and sanitize all fields
+    body('name', 'Name is required').trim().isLength({min: 3}).escape(),
+    body('description', 'Description is required').trim().isLength({min: 10}).escape(),
+    //After validation continue
+    (req, res, next) => {
+        //Extract validation errors
+        const errors = validationResult(req);
+        
+        //Create new category instance
+        let category = new Category({
+            name: req.body.name,
+            description: req.body.description,
+            _id: req.params.id
+        })
+        if(!errors.isEmpty()) {
+            //there are errors, render form again with sanitazed values
+            res.render('category_form', {title: 'Create category', category: category, action: 'Update'})
+            console.log(errors)
+            return
+        } else {
+            //Data from form is valid
+            Category.findByIdAndUpdate(req.params.id, category, {}, function (err,category) {
+                if(err) { return next(err); }
+                //Success
+                res.redirect('/catalog')
+            })
+        }
+        
+    }
+
+]
 
 exports.category_delete_get = function(req,res, next) {
-    res.send('to be implemented')
+    async.parallel({
+        category: function(callback){
+            Category.findById(req.params.id).exec(callback)
+        },
+        coins: function(callback){
+            Coin.find({'category': req.params.id}).exec(callback)
+        }
+        
+    }, function(err, results) {
+        if(err) {return next(err)}
+        if(results.category === null) {
+            res.redirect('/catalog/categories')
+        }
+        res.render('category_delete', {title: 'Delete Category', coins: results.coins, category: results.category})
+    })
 }
 
 exports.category_delete_post = function(req,res, next) {
-    res.send('to be implemented')
+    Category.findByIdAndDelete(req.body.categoryid)
+        .exec(function(err){
+            if(err) {return next(err)}
+            res.redirect('/catalog/categories')
+        })
 }
 
 exports.category_detail = function(req,res, next) {
-    res.send('to be implemented')
+    async.parallel({
+        category: function(callback){
+            Category.findById(req.params.id).exec(callback)
+        },
+        coins: function(callback){
+            Coin.find({'category': req.params.id}).exec(callback)
+    
+        }
+    }, function(err, results) {
+        if(err) {return next(err)}
+        if(results.category === null) {
+            res.redirect('/')
+        }
+        res.render('category_detail', {title: results.category.name, category: results.category, coins: results.coins})
+    })
 }
 
 exports.category_list = function(req, res, next) {
-    res.send('to be implemented')
+    res.redirect('/')
 }
